b7980b124e31fbb6940ea16cf9d39d2bfc2a6898
unknown <joaquimmigueldecristo@gmail.com>
2025-07-07 18:48:13 +0200

    first commit 1995

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,4 @@ window.addEventListener("scroll", function(){
   let header = this.document.querySelector('#headerX')
   header.classList.toggle('sticky-top', window.scrollY > 0 )
 })
-
-
-
-
-
- 
+   
--- assets/js/view/home/home.js
+++ assets/js/view/home/home.js
@@ -1,8 +1,8 @@
 import get_template from '../../components/get_template.js'
- 
+
 
 export default {
- 
+
 
   data: function () {
     return {
@@ -16,22 +16,67 @@ export default {
   },
 
   async mounted() {
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
+    
+    
+ 
+
+    var swiper = new Swiper(".swiper", {
+      loop: true,
+      grabCursor: true,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 18
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 18
+        },
+        1188: {
+          slidesPerView: 6,
+          spaceBetween: 24
+        }
+      }
     });
+
+  var swiper = new Swiper(".swiper_testemuno", {
+      loop: true,
+      grabCursor: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 18
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 18
+        },
+        1188: {
+          slidesPerView: 6,
+          spaceBetween: 24
+        }
+      }
+    });
+
+
+
+
   },
 
   template: await get_template('./assets/js/view/home/home')
